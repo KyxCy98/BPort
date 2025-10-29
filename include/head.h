6b440f598af2160bfec73d78a8676b4f6960664e
lyxsec <119648634+KyxCy98@users.noreply.github.com>
2025-10-29 15:54:25 +0700

    Add header file with standard includes

---
+++ include/head.h
@@ -0,0 +1,22 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <netdb.h>
+#include <time.h>
+
+#endif
